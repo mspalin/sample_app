bd65503772166d56259f811a23ebfd1f31584a97
Mike Spalinger <mike.spalinger@gmail.com>
2012-07-21 21:29:01 -0600

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 SampleApp::Application.routes.draw do  # The priority is based upon order of creation:
-  get "static_pages/home"
-  get "static_pages/help"
-  get "static_pages/about"
+  root to: 'static_pages#home'
+
+  get "users/new"
+  match '/signup',  to: 'users#new'
+
+  match '/help',    to: 'static_pages#help'
+  match '/about',   to: 'static_pages#about'
+  match '/contact', to: 'static_pages#contact'
 
   # first created -> highest priority.
 
