b56a702edb3e48ee5f3b5b8e5f4506556d66910d
Mike Spalinger <mike.spalinger@gmail.com>
2012-08-06 14:01:30 -0600

    Add user following

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 SampleApp::Application.routes.draw do  # The priority is based upon order of creation:
-  resources :users
-  resources :sessions,   only: [:new, :create, :destroy]
-  resources :microposts, only: [:create, :destroy]
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
+
+  resources :sessions,      only: [:new, :create, :destroy]
+  resources :microposts,    only: [:create, :destroy]
+  resources :relationships, only: [:create, :destroy]
 
   root to: 'static_pages#home'
 
